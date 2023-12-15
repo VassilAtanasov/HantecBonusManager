2f971e26fe228dacb8c72da354d47464c8883225
Vas <vassil.net@gmail.com>
2023-12-15 18:55:13 +0200

    Resolve ProcessBonusForAccounts_OnEmptyAccountList_ReturnListOfProcessResults

--- HantecBonusManager/BonusManager.cs
+++ HantecBonusManager/BonusManager.cs
@@ -3,11 +3,38 @@ using HantecBonusManager.Services;
 
 namespace HantecBonusManager
 {
-    public class BonusManager() : IBonusManager
+    public class BonusManager(ITradingPlatformApi tradingPlatformApi, IBonusCalculator bonusCalculator) : IBonusManager
     {
         public async Task<List<ProcessResults>> ProcessBonusForAccounts()
         {
-            return new List<ProcessResults>();
+            var results = new List<ProcessResults>();
+            var accounts = await tradingPlatformApi.GetAccountsList() ?? new List<Account>();
+            foreach (var account in accounts)
+            {
+                if (account is not null)
+                {
+                    List<Deal> deals = await tradingPlatformApi.GetHistoricalDeals(account.Id, DateTime.Now.AddMonths(-1), DateTime.Now);
+                    decimal totalBonus = await CalculateTotalBonus(deals);
+
+                    await tradingPlatformApi.CreateCreditOperation(account.Id, totalBonus);
+                    results.Add(new ProcessResults() { AccountId = account.Id, Amount = totalBonus });
+                }
+            }
+
+            return results;
+        }
+
+        private async Task<decimal> CalculateTotalBonus(List<Deal> deals)
+        {
+            decimal totalBonus = 0;
+
+            foreach (var deal in deals)
+            {
+                BonusPoint bonus = await bonusCalculator.CalculateBonus(deal);
+                totalBonus += bonus.Amount;
+            }
+
+            return totalBonus;
         }
     }
 }
