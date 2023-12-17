c3aa71e4aba06238678e5dc967e30c1466b4c668
Vas <vassil.net@gmail.com>
2023-12-17 12:44:36 +0200

    Updated bonus logic in AdvancedBonusCalculationStrategy

--- HantecBonusManager/Strategy/AdvancedBonusCalculationStrategy.cs
+++ HantecBonusManager/Strategy/AdvancedBonusCalculationStrategy.cs
@@ -4,7 +4,7 @@ public class AdvancedBonusCalculationStrategy : IBonusCalculationStrategy
     public BonusPoint CalculateBonus(Deal deal)
     {
         // Advanced bonus calculation logic
-        decimal bonusAmount = deal.DealCount * 0.5m;
+        decimal bonusAmount = deal.DealCount * 1.0m;
 
         return new BonusPoint { Amount = bonusAmount };
     }
