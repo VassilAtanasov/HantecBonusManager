225f7b842ae138ae2a0a324036a2a7166eee2aa4
Vas <vassil.net@gmail.com>
2023-12-18 13:01:09 +0200

    implemented AddDeals

--- HantecBonusManager/Data/DealRepository.cs
+++ HantecBonusManager/Data/DealRepository.cs
@@ -5,6 +5,7 @@ namespace HantecBonusManager.Data
     public interface IDealRepository
     {
         List<Deal> GetHistoricalDeals(long accountId, DateTime fromDate, DateTime toDate);
+        void AddDeals(List<Deal> deals); // New method for adding a list of deals
     }
 
     public class DealRepository : IDealRepository
@@ -21,6 +22,12 @@ namespace HantecBonusManager.Data
             return _dbContext.Deals
                 .Where(deal => deal.AccountId == accountId && deal.Date >= fromDate && deal.Date <= toDate)
                 .ToList();
+        }
+
+        public void AddDeals(List<Deal> deals)
+        {
+            _dbContext.Deals.AddRange(deals);
+            _dbContext.SaveChanges();
         }
     }
 }
